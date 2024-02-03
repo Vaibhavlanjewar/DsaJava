645a963d64ac0679932d6b2093137a7d1236543d
Vaibhav Nivrutti Lanjewar <105924348+vaibhavlanjewar@users.noreply.github.com>
2024-02-03 21:48:07 +0530

    Create Binary-number-in-linkedlist-to-decimal all

---
+++ String1.java
@@ -0,0 +1,79 @@
+import java.util.*;
+public class String1 {
+    public static void main (String args[]){
+
+        System.out.println("Strings are immutable, i.e once we have make string , it created inside memo and we cant change and delete\n");
+
+        //     class    obj def
+        Scanner sc= new Scanner(System.in);
+
+
+//        String name="vnbl";
+//        String fullname="Vaibhav lanjewar";
+//        String sent="My name is vaibhav lanjewar";
+
+
+
+//        System.out.println("Enter you name :");
+//        // for single word , next()
+//        String instr=sc.next();
+//        System.out.println("you name is "+ instr);
+
+//        System.out.println("Enter you full name:");
+//        // for complete line , nextLine
+//        String inst=sc.nextLine();
+//        System.out.println("you name is "+ inst);
+
+//        // concatination two string join or add
+//        String fn="tony";
+//        String ln="Stark";
+//        String fuln=fn+" "+ln;
+//        System.out.println(fuln);
+//
+//          // length function
+//        System.out.println("Length of full name = "+ fuln.length());
+
+
+        // if we have to print all char of string
+        // using charAt
+//        for(int i=0;i<fuln.length();i++){
+//            System.out.println(fuln.charAt(i));  // charAt(index)
+//        }
+
+
+//        // # compare two string
+//        String name1="vaibhav";
+//        String name2="vaibhav";
+//// in strings char b has more value than a and so on...
+//        // if s1==s2 : 0
+////        if  s1> s2 : +ve
+////          if  s1<s2  : -ve
+//        if(name1.compareTo(name2)==0){
+//            System.out.println("Strings are equal ");
+//        }
+//        else{
+//            System.out.println("strings are not equal ");
+//        }
+
+//        // this code generating wrong ans when we make string by primitive way
+//        if(new String("Hi")==new String("Hi")){
+//            System.out.println("Strings are equal ");
+//        }
+//        else{
+//            System.out.println("strings are not equal ");
+//        }
+
+//        // # substrings
+//        String sent="My name is tonny";
+//        String name=sent.substring(11,sent.length());
+//        System.out.println(name);
+//
+//        String sent1="tonnyStrak";
+//        String name1=sent1.substring(0,4);
+//        System.out.println(name);
+
+
+
+
+    }
+}
