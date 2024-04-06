972d59ebb72b2d06c191094380dc972fe43047f2
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-04-06 11:01:00 +0530

    BackTracking Started 06/04/2024

---
+++ BackTracking/ArrAy_Backtrack.java
@@ -0,0 +1,30 @@
+package BackTracking;
+
+public class ArrAy_Backtrack {
+    public static void changeArr(int arr[],int i,int val){
+    // base case 
+      if(i==arr.length){
+        printArr(arr);
+        return ;
+      }
+
+      // recursion // kaam 
+      arr[i]=val;
+      changeArr(arr, i+1, val+1);
+
+      arr[i]=arr[i]-2; // BackTracking 
+
+    }
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[]=new int[5];
+        changeArr(arr,0,1);
+        printArr(arr);
+    }
+}
