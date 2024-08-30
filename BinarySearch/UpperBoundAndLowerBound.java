3d8aea3770cdd02880719319268d722d104d0834
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-08-30 12:13:59 +0530

    Upper and lower bound

--- BinarySearch/UpperBoundAndLowerBound.java
+++ BinarySearch/UpperBoundAndLowerBound.java
@@ -53,12 +53,13 @@ public class UpperBoundAndLowerBound {
         int  ans=-1;
        while(s<=e){
         int mid=(s+e)/2;
-         if(arr[mid]<x){
+         if(arr[mid]>=
+         x){
           ans=arr[mid];
-           s=mid+1;
+           e=mid-1;
            }
            else{ 
-             e=mid-1;    
+             s=mid+1;    
             }
         
        } 
