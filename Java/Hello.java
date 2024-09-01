c2604916252695c6e49674f6537101e07b1fb6b2
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-09-01 18:58:23 +0530

    java

---
+++ Java/Hello.java
@@ -0,0 +1,74 @@
+
+
+public class Hello{
+    public static void main(String args[]){
+        // System.out.println("Hello todays is 01/09/2024");
+
+        // System.out.println(3+5);
+        // System.out.println(2*3);
+        // int num=3;
+        // int num2=2;
+        // int result=num+num2;
+        // System.out.println(result);
+
+
+        
+// -------------------------------------------
+        // variable and datatype 
+    
+        // int a=5;
+        // // byte b=129; //lossy conversion 
+        // byte bt=127;
+        // short sh=558;
+        // float kg=5.2f; //floate kg=5.6 ;it is double 
+        // long l=5854l; 
+        // double db=5.66;
+        // char c='k';
+        // boolean b=true;
+
+// -----------------------------------
+        // literals
+         // bin
+        // int num1=0b101; //binary 101--5
+        // System.out.println(num1); //5
+ 
+        // // hex
+        // int num2=0x7E;
+        // System.out.println(num2); //126
+
+        // int num=10_0_0_001;
+        // System.out.println(num); //1000001
+
+        // double x=56;
+        // System.out.println(x);
+
+        // double x1=12e10;
+        // System.out.println(x1); //1.2E11
+  
+        // char a='a';
+        // a++;
+        // System.out.println(a); //b
+// ----------------------------------------------.
+   
+    //  type conversion 
+    // byte b=127;
+    // int a=b;  //byte to int 
+    // System.out.println(a);
+
+    // int a=257; 
+    // byte k=(byte)a; //1  , it perfoms mod operation , a%256 , 256 is range of byte
+    // System.out.println(k);
+
+    // float f=5.6f;
+    // int i=(int)f;
+    // System.out.println(i);
+
+    // promated value
+    // byte a=10;
+    // byte b=30;
+    // int res=a*b;
+    // System.out.println(res);
+
+  
+    }   
+}
