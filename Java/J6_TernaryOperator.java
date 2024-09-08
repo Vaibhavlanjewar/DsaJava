9caf7187e00f5963be58add6f63a7c0f5a5ea1bd
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-09-09 00:39:02 +0530

    Java ternary operator , Switch case

---
+++ Java/J6_TernaryOperator.java
@@ -0,0 +1,9 @@
+public class J6_TernaryOperator {
+    public static void main(String args[]){
+        int a=10;
+        // check number is odd or even 
+        String res=a%2==0?"Even":"odd";
+        System.out.println(res);
+        
+    }
+}
