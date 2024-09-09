f314e6dbe8ebacb59a2773daf4be212e4a8f41e4
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-09-09 23:57:22 +0530

    Java Loops

---
+++ Java/J7_WhileLoop.java
@@ -0,0 +1,9 @@
+public class J7_WhileLoop {
+    public static void main(String args[]){
+        int i=1;
+        while(i<=5){
+            System.out.println("Loop ,count:"+i);
+            i++;
+        }
+    }
+}
---
+++ Java/J8_DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class J8_DoWhileLoop {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5);
+
+    }
+}
