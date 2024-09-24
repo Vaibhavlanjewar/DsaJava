1e974a85940c89eb5101066d4c64d080133948e2
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-09-24 19:09:53 +0530

    Todays commit

--- Matrix/SpiralMatrix.java
+++ Matrix/SpiralMatrix.java
@@ -51,6 +51,7 @@ public class SpiralMatrix {
                         {9,10,11,12},
                         {13,14,15,16},
                     };
+                    print(matrix);
                    printSpral(matrix);
       
     }
