27987738bdf73a5f7386b79a84fb094d258fbaae
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-09-26 22:18:33 +0530

    TCS , Parking with most spaces full

--- TCS-NQT/q4.java
+++ TCS-NQT/q4.java
@@ -1,6 +1,7 @@
-/* A parking lot in a mall has RxC number of parking spaces. Each parking space will either be  empty(0) or full(1). The status (0/1) of a parking space is represented as the element of the matrix. The task is to find index of the prpeinzta row(R) in the parking lot that has the most of the parking spaces full(1).
+/* A parking lot in a mall has RxC number of parking spaces. Each parking space will either be  empty(0) or full(1). The status (0/1) of a parking space is represented as the element of the matrix. The task is to find index of the  row(R) in the parking lot that has the most of the parking spaces full(1).
 
 Note :
+
 RxC- Size of the matrix
 Elements of the matrix M should be only 0 or 1.
 
@@ -22,29 +23,41 @@ Output :
 // https://prepinsta.com/tcs-coding-questions/
 
 import java.util.*;
+
 public class q4 {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int row=sc.nextInt();
-        int col=sc.nextInt();
-        
-        int[][]arr=new int[row][col];
-        int max=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-             arr[i][j]=sc.nextInt();
- 
-               } 
-            }
 
-        for(int i=0;i<row;i++){
-          for(int j=0;j<col;j++){
-            if(arr[i][j]==1){
-                max=Math.max(max,i);
-                }
-             } 
-          }
-        System.out.println("Row " + row + "Has max number of 1's");
+  public static int maxLength(int arr[][], int r, int c) {
+    int max1 = 0;
+    int row=0;
+    
+    for (int i = 0; i < r; i++) {
+      int cnt = 0;
+      for (int j = 0; j < c; j++) {
+        if (arr[i][j] != 0) {
+            cnt++;
+        }        
+      }
+      if(cnt>max1){
+      max1=Math.max(max1,cnt);
+       row=i;
+      }
     }
-}
+    return row+1;
+  }
 
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int r = sc.nextInt();
+    int c = sc.nextInt();
+    int arr[][] = new int[r][c];
+
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        arr[i][j] = sc.nextInt();
+      }
+    }
+
+    System.out.println(maxLength(arr, r, c));
+
+  }
+}
